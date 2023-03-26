408e194171c806e7f8a561f91927b9bf669b9895
Fernando Romero <fer.rom.mu@gmail.com>
2023-03-26 18:15:54 -0300

    Creates DataService and DataServiceImpl files.

---
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/DataServiceImpl.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.eperdemic.services.impl
+
+import ar.edu.unq.eperdemic.services.DataService
+
+class DataServiceImpl: DataService {
+    override fun deleteAll() {
+        TODO("Not yet implemented")
+    }
+
+    override fun crearSetDatosIniciales() {
+        TODO("Not yet implemented")
+    }
+}
