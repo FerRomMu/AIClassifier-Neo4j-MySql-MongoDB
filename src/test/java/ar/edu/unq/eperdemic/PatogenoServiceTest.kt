ead54d06b918a8391a05c155ac6abe4eeb881620
Fernando Romero <fer.rom.mu@gmail.com>
2023-03-29 18:13:52 -0300

    Quito imports de mas.

--- src/test/java/ar/edu/unq/eperdemic/PatogenoServiceTest.kt
+++ src/test/java/ar/edu/unq/eperdemic/PatogenoServiceTest.kt
@@ -1,39 +1,23 @@
 package ar.edu.unq.eperdemic
 
-import ar.edu.unq.eperdemic.modelo.Especie
 import ar.edu.unq.eperdemic.modelo.Patogeno
 import ar.edu.unq.eperdemic.persistencia.dao.PatogenoDAO
-import ar.edu.unq.eperdemic.persistencia.dao.jdbc.JDBCDataDAO
-import ar.edu.unq.eperdemic.persistencia.dao.jdbc.JDBCPatogenoDAO
-import ar.edu.unq.eperdemic.services.PatogenoService
-import ar.edu.unq.eperdemic.services.impl.DataServiceImpl
 import ar.edu.unq.eperdemic.services.impl.PatogenoServiceImpl
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
-import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.junit.jupiter.MockitoExtension
 
-
-//@TestInstance(PER_CLASS)
-//@RunWith(MockitoJUnitRunner::class)
 
 @ExtendWith(MockitoExtension::class)
 class PatogenoServiceTest {
 
     @Mock
     lateinit var patogenoDAO : PatogenoDAO;
-
     lateinit var patogeno :Patogeno;
 
     @InjectMocks
