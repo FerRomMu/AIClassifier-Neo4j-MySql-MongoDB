874b0e42f8b6b84459bf0b550f97951deb1f8874
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2023-04-07 14:05:35 -0300

    mtodo recuperar todos

--- src/main/java/ar/edu/unq/eperdemic/persistencia/dao/hibernate/HibernateVectorDAO.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/dao/hibernate/HibernateVectorDAO.kt
@@ -2,11 +2,18 @@ package ar.edu.unq.eperdemic.persistencia.dao.hibernate
 
 import ar.edu.unq.eperdemic.modelo.Vector
 import ar.edu.unq.eperdemic.persistencia.dao.VectorDAO
+import ar.edu.unq.eperdemic.services.runner.TransactionRunner
 
 class HibernateVectorDAO: HibernateDAO<Vector>(Vector::class.java), VectorDAO {
 
     override fun recuperarTodos(): List<Vector> {
-        TODO("Not yet implemented")
+        val session = TransactionRunner.currentSession
+
+        val hql = "from vector"
+
+        val query = session.createQuery(hql, Vector::class.java)
+
+        return query.resultList
     }
 
 }
