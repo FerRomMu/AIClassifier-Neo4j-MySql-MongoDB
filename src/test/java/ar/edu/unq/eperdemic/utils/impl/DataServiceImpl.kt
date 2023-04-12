c4d8329cc549b3d30b513610cd348e1387bcef15
Fernando Romero <fer.rom.mu@gmail.com>
2023-04-12 12:18:09 -0300

    Se desacopla constructor de dataService. Se agrega vectores al data set inicial.

--- src/test/java/ar/edu/unq/eperdemic/utils/impl/DataServiceImpl.kt
+++ src/test/java/ar/edu/unq/eperdemic/utils/impl/DataServiceImpl.kt
@@ -1,11 +1,22 @@
 package ar.edu.unq.eperdemic.utils.impl
 
 import ar.edu.unq.eperdemic.modelo.Patogeno
+import ar.edu.unq.eperdemic.modelo.TipoDeVector
+import ar.edu.unq.eperdemic.modelo.Ubicacion
+import ar.edu.unq.eperdemic.modelo.Vector
 import ar.edu.unq.eperdemic.persistencia.dao.DataDAO
 import ar.edu.unq.eperdemic.persistencia.dao.PatogenoDAO
+import ar.edu.unq.eperdemic.persistencia.dao.VectorDAO
+import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateDataDAO
+import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernatePatogenoDAO
+import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateVectorDAO
 import ar.edu.unq.eperdemic.utils.DataService
 
-class DataServiceImpl(val patogenoDao: PatogenoDAO, val dataDao: DataDAO): DataService {
+class DataServiceImpl(): DataService {
+
+    val patogenoDao: PatogenoDAO = HibernatePatogenoDAO()
+    val vectorDao: VectorDAO = HibernateVectorDAO()
+    val dataDao: DataDAO = HibernateDataDAO()
 
     override fun eliminarTodo() {
         dataDao.clear()
@@ -19,6 +30,14 @@ class DataServiceImpl(val patogenoDao: PatogenoDAO, val dataDao: DataDAO): DataS
             patogeno.tipo = "Tipo $i"
             patogeno.cantidadDeEspecies = i
             patogenoDao.guardar(patogeno)
+        }
+
+        val vector = Vector(TipoDeVector.Insecto)
+        for (i in 0 .. 20) {
+            vector.id = null
+            vector.ubicacion = Ubicacion("Lugar $i")
+            vector.tipo = listOf<TipoDeVector>(TipoDeVector.Insecto, TipoDeVector.Animal, TipoDeVector.Persona)[i % 3]
+            vectorDao.guardar(vector)
         }
     }
 }
