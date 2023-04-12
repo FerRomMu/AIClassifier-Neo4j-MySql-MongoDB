a45c85d0bd4bd7a2f4461c1e00e8c5da176bccd1
Fernando Romero <fer.rom.mu@gmail.com>
2023-04-12 12:38:34 -0300

    Se agrega ubicaciones al set de datos

--- src/test/java/ar/edu/unq/eperdemic/utils/impl/DataServiceImpl.kt
+++ src/test/java/ar/edu/unq/eperdemic/utils/impl/DataServiceImpl.kt
@@ -6,16 +6,20 @@ import ar.edu.unq.eperdemic.modelo.Ubicacion
 import ar.edu.unq.eperdemic.modelo.Vector
 import ar.edu.unq.eperdemic.persistencia.dao.DataDAO
 import ar.edu.unq.eperdemic.persistencia.dao.PatogenoDAO
+import ar.edu.unq.eperdemic.persistencia.dao.UbicacionDAO
 import ar.edu.unq.eperdemic.persistencia.dao.VectorDAO
 import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateDataDAO
 import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernatePatogenoDAO
+import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateUbicacionDAO
 import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateVectorDAO
+import ar.edu.unq.eperdemic.services.runner.TransactionRunner
 import ar.edu.unq.eperdemic.utils.DataService
 
 class DataServiceImpl(): DataService {
 
     val patogenoDao: PatogenoDAO = HibernatePatogenoDAO()
     val vectorDao: VectorDAO = HibernateVectorDAO()
+    val ubicacionDao: UbicacionDAO = HibernateUbicacionDAO()
     val dataDao: DataDAO = HibernateDataDAO()
 
     override fun eliminarTodo() {
@@ -24,20 +28,24 @@ class DataServiceImpl(): DataService {
 
     override fun crearSetDeDatosIniciales() {
 
-        val patogeno = Patogeno("Tipo 0")
-        for (i in 0..20){
-            patogeno.id = null
-            patogeno.tipo = "Tipo $i"
-            patogeno.cantidadDeEspecies = i
-            patogenoDao.guardar(patogeno)
+        TransactionRunner.runTrx {
+            for (i in 0..20){
+                var patogeno = Patogeno("Tipo $i")
+                patogeno.tipo = "Tipo $i"
+                patogeno.cantidadDeEspecies = i
+                patogenoDao.guardar(patogeno)
+            }
+
+            for (i in 0 .. 20) {
+                var vector = Vector(
+                    listOf(TipoDeVector.Insecto, TipoDeVector.Animal, TipoDeVector.Persona)[i % 3]
+                )
+                var ubicacion = Ubicacion("Lugar $i")
+                vector.ubicacion = ubicacion
+                ubicacionDao.guardar(ubicacion)
+                vectorDao.guardar(vector)
+            }
         }
 
-        val vector = Vector(TipoDeVector.Insecto)
-        for (i in 0 .. 20) {
-            vector.id = null
-            vector.ubicacion = Ubicacion("Lugar $i")
-            vector.tipo = listOf<TipoDeVector>(TipoDeVector.Insecto, TipoDeVector.Animal, TipoDeVector.Persona)[i % 3]
-            vectorDao.guardar(vector)
-        }
     }
 }
