39dd85aaa694e6d4ebf3426af1cc0625812e8901
Fernando Romero <fer.rom.mu@gmail.com>
2023-04-12 12:07:32 -0300

    Se quito dataservice JDBC para agregar una impl.

--- src/test/java/ar/edu/unq/eperdemic/utils/impl/DataServiceImpl.kt
+++ src/test/java/ar/edu/unq/eperdemic/utils/impl/DataServiceImpl.kt
@@ -1,28 +1,24 @@
-package ar.edu.unq.eperdemic.utils.jdbc
+package ar.edu.unq.eperdemic.utils.impl
 
 import ar.edu.unq.eperdemic.modelo.Patogeno
 import ar.edu.unq.eperdemic.persistencia.dao.DataDAO
 import ar.edu.unq.eperdemic.persistencia.dao.PatogenoDAO
 import ar.edu.unq.eperdemic.utils.DataService
 
-class DataServiceJDBC (val patogenoDao: PatogenoDAO, val dataDao: DataDAO): DataService {
+class DataServiceImpl(val patogenoDao: PatogenoDAO, val dataDao: DataDAO): DataService {
 
     override fun eliminarTodo() {
         dataDao.clear()
     }
 
     override fun crearSetDeDatosIniciales() {
-        /*
+
         val patogeno = Patogeno("Tipo 0")
         for (i in 0..20){
             patogeno.id = null
             patogeno.tipo = "Tipo $i"
             patogeno.cantidadDeEspecies = i
-            patogenoDao.crear(patogeno)
-
-
+            patogenoDao.guardar(patogeno)
         }
-
-         */
     }
 }
