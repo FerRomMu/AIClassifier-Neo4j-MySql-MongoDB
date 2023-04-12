ad5751ce858e2b9ce42a1da4b593fb05bb212d93
Fernando Romero <fer.rom.mu@gmail.com>
2023-04-12 15:30:47 -0300

    Se quito codigo repetido de daos.

--- src/main/java/ar/edu/unq/eperdemic/persistencia/dao/hibernate/HibernateUbicacionDAO.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/dao/hibernate/HibernateUbicacionDAO.kt
@@ -5,18 +5,4 @@ import ar.edu.unq.eperdemic.modelo.Vector
 import ar.edu.unq.eperdemic.persistencia.dao.UbicacionDAO
 import ar.edu.unq.eperdemic.services.runner.TransactionRunner
 
- open class HibernateUbicacionDAO: HibernateDAO<Ubicacion>(Ubicacion::class.java), UbicacionDAO{
-
-  override fun recuperarTodos(): List<Ubicacion> {
-      val session = TransactionRunner.currentSession
-
-      val hql = "from Ubicacion"
-
-      val query = session.createQuery(hql, Ubicacion::class.java)
-
-   return query.resultList
-  }
-
-
- }
-
+ class HibernateUbicacionDAO: HibernateDAO<Ubicacion>(Ubicacion::class.java), UbicacionDAO
