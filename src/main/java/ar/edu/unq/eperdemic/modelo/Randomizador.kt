2a037a6e1c0dd9d4ef76c6d68aa3a68dec9bdaee
Juan ignacio Garcia <juan.ignacio.garcia.02@gmail.com>
2023-04-15 18:54:53 -0300

    Se modifico Randomizador y se implemento los test

--- src/main/java/ar/edu/unq/eperdemic/modelo/Randomizador.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/Randomizador.kt
@@ -5,7 +5,7 @@ import kotlin.random.Random
 
 class Randomizador {
     private var instance : Randomizador? = null
-    var estado = EstadoRandomizadorAlatoria()
+    var estado : EstadoRandomizador = EstadoRandomizadorAlatorio()
 
     fun  getInstance(): Randomizador {
         if (instance == null)  // NOT thread safe!
@@ -18,19 +18,20 @@ class Randomizador {
         return estado.valor(valorMin,valorMax)
     }
 
+
 }
 
-abstract class  EstadoRandomizador (){
+abstract class  EstadoRandomizador(){
     abstract fun valor(valorMin:Int, valorMax:Int) : Int
 }
 
-class EstadoRandomizadorDeterminístico () : EstadoRandomizador() {
+class EstadoRandomizadorDeterminístico() : EstadoRandomizador() {
     override fun valor(valorMin:Int, valorMax:Int) : Int{
         return valorMax
     }
 }
 
-class EstadoRandomizadorAlatoria () : EstadoRandomizador(){
+class EstadoRandomizadorAlatorio() : EstadoRandomizador(){
     override fun valor(valorMin:Int, valorMax:Int): Int{
         return Random.nextInt(valorMin,valorMax+1)
     }
--- src/test/java/ar/edu/unq/eperdemic/services/impl/UbicacionServiceImplTest.kt
+++ src/test/java/ar/edu/unq/eperdemic/services/impl/UbicacionServiceImplTest.kt
@@ -1,5 +1,6 @@
 package ar.edu.unq.eperdemic.services.impl
 
+import ar.edu.unq.eperdemic.modelo.*
 import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateUbicacionDAO
 import ar.edu.unq.eperdemic.persistencia.dao.hibernate.HibernateVectorDAO
 import org.junit.jupiter.api.AfterEach
@@ -10,17 +11,49 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class UbicacionServiceImplTest {
 
+    lateinit var vectorService: VectorServiceImpl
+    lateinit var vectorDAO: HibernateVectorDAO
+
     lateinit var ubicacionService: UbicacionServiceImpl
     lateinit var ubicacionDAO: HibernateUbicacionDAO
+    lateinit var dado: Randomizador
 
     @BeforeEach
     fun setUp() {
          ubicacionDAO = HibernateUbicacionDAO()
          ubicacionService = UbicacionServiceImpl(ubicacionDAO)
+
+         vectorDAO = HibernateVectorDAO()
+         vectorService = VectorServiceImpl(vectorDAO,ubicacionDAO)
+
+         dado.getInstance().estado = EstadoRandomizadorDeterminístico()
     }
 
     @Test
-    fun mover() {
+    fun  `mover vector a una ubicación con un humano y un animal`() {
+        var cordoba = ubicacionService.crearUbicacion("Cordoba")
+        var chaco = ubicacionService.crearUbicacion("Chaco")
+
+        var vectorAMover = vectorService.crearVector(TipoDeVector.Persona,cordoba.id!!)
+
+        var vectorVictima1 = vectorService.crearVector(TipoDeVector.Persona,chaco.id!!)
+        var vectorVictima2 = vectorService.crearVector(TipoDeVector.Animal,chaco.id!!)
+
+        var patogeno = Patogeno("Patogeni_SS")
+        var especieAContagiar = Especie(patogeno,"Especie_Sl","Honduras")
+
+        vectorService.infectar(vectorAMover,especieAContagiar)
+
+        assertEquals(vectorAMover.especiesContagiadas.size,1)
+        assertEquals(vectorVictima1.especiesContagiadas.size,0)
+        assertEquals(vectorVictima2.especiesContagiadas.size,0)
+
+        ubicacionService.mover(vectorAMover.id!!,chaco.id!!)
+
+        assertEquals(vectorAMover.especiesContagiadas.size,1)
+        assertEquals(vectorVictima1.especiesContagiadas.size,1)
+        assertEquals(vectorVictima2.especiesContagiadas.size,0)
+
     }
 
     @Test
