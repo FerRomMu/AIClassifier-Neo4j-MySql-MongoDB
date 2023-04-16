9cb62594978658e27936ae43f3767989227ba82b
Juan Cruz Insaurralde <juancruzinsaurralde@outlook.com>
2023-04-16 14:16:08 -0300

    logica para agregarEspecie

--- src/main/java/ar/edu/unq/eperdemic/persistencia/dao/hibernate/HibernatePatogenoDAO.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/dao/hibernate/HibernatePatogenoDAO.kt
@@ -1,6 +1,27 @@
 package ar.edu.unq.eperdemic.persistencia.dao.hibernate
 
+import ar.edu.unq.eperdemic.exceptions.DataNotFoundException
 import ar.edu.unq.eperdemic.modelo.Patogeno
+import ar.edu.unq.eperdemic.modelo.Vector
 import ar.edu.unq.eperdemic.persistencia.dao.PatogenoDAO
+import ar.edu.unq.eperdemic.services.runner.TransactionRunner
 
-class HibernatePatogenoDAO : HibernateDAO<Patogeno>(Patogeno::class.java), PatogenoDAO
+class HibernatePatogenoDAO : HibernateDAO<Patogeno>(Patogeno::class.java), PatogenoDAO {
+
+    override fun vectorAleatorioEn(ubicacionId: Long): Vector {
+        val session = TransactionRunner.currentSession
+
+        val hql = """
+            from Vector v 
+            where v.ubicacion.id = :idUbi
+            order by rand()
+            limit 1
+            """
+
+        val query = session.createQuery(hql, Vector::class.java)
+
+        query.setParameter("idUbi", ubicacionId)
+
+        return query.singleResult ?: throw DataNotFoundException("No hay vectores en la ubicacion dada")
+    }
+}
