32de8e17104c86df5e5612dddc96dbcb82927a12
Juan Cruz Insaurralde <juancruzinsaurralde@outlook.com>
2023-04-19 17:31:43 -0300

    agrego tests para esPandemia donde se cumple para una especie y otro en que no

--- src/test/java/ar/edu/unq/eperdemic/utils/DataService.kt
+++ src/test/java/ar/edu/unq/eperdemic/utils/DataService.kt
@@ -1,10 +1,13 @@
 package ar.edu.unq.eperdemic.utils
 
+import ar.edu.unq.eperdemic.modelo.Especie
+
 interface DataService {
     /**
      * Crea un set de datos iniciales.
      */
     fun crearSetDeDatosIniciales()
+    fun crearPandemiaPositiva(): Especie
 
     /**
      * Elimina toda la información persistida en la base de datos.
