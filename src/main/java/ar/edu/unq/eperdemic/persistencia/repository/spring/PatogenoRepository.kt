ef57ddabc9f9afafd8ec9817f906a50278852f23
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2023-05-01 12:12:33 -0300

    arme las querys de esPandemia y especiesDePatogeno

--- src/main/java/ar/edu/unq/eperdemic/persistencia/repository/spring/PatogenoRepository.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/repository/spring/PatogenoRepository.kt
@@ -8,11 +8,24 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface PatogenoRepository : CrudRepository<Patogeno,Long> {
-    /*@Query()
+
+    @Query(
+        "SELECT ES FROM PATOGENO P" +
+                "JOIN P.ESPECIES ES" +
+                "WHERE P.ID = 1? "
+    )
     fun especiesDePatogeno(id: Long?): List<Especie>
 
-    @Query()
+
+    @Query(
+        "SELECT (COUNT(DISTINCT V.UBICACION.ID) * 2) >= (SELECT COUNT(*) FROM UBICACION) AS CANTIDAD_UBICACIONES_MAYOR_A_LA_MITAD " +
+                "FROM VECTOR V" +
+                "JOIN V.ESPECIESCONTAGIADAS ES" +
+                "WHERE ES.ID = 1?"
+    )
     fun esPandemia(especieId: Long): Boolean
-     */
+
+
+
 }
 //TODO esPandemia y especieDePatogeno
