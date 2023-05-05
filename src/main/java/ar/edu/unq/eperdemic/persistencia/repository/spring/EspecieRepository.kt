4deb867496a2dde62e4c553dd53066105161773a
Fernando Romero <fer.rom.mu@gmail.com>
2023-05-05 15:09:22 -0300

    Se agrega metodo cantidadDeInfectados en especie repository.

--- src/main/java/ar/edu/unq/eperdemic/persistencia/repository/spring/EspecieRepository.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/repository/spring/EspecieRepository.kt
@@ -1,8 +1,17 @@
 package ar.edu.unq.eperdemic.persistencia.repository.spring
 
 import ar.edu.unq.eperdemic.modelo.Especie
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface EspecieRepository: CrudRepository<Especie, Long>
+interface EspecieRepository: CrudRepository<Especie, Long> {
+
+    @Query("SELECT COUNT(*)\n" +
+            "FROM Especie es\n" +
+            "JOIN es.vectores e\n" +
+            "WHERE es.id = :idEspecie\n")
+    fun cantidadDeInfectados(@Param("idEspecie") especieId: Long): Int
+}
