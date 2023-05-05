9501e54a8c9c24cc1b48dcd15e264c7b17b957fa
Fernando Romero <fer.rom.mu@gmail.com>
2023-05-05 00:30:29 -0300

    Agrego query de vectoresEn.

--- src/main/java/ar/edu/unq/eperdemic/persistencia/repository/spring/UbicacionRepository.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/repository/spring/UbicacionRepository.kt
@@ -1,11 +1,16 @@
 package ar.edu.unq.eperdemic.persistencia.repository.spring
 
 import ar.edu.unq.eperdemic.modelo.Ubicacion
+import ar.edu.unq.eperdemic.modelo.Vector
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UbicacionRepository : CrudRepository<Ubicacion, Long> {
-
-    //fun vectoresEn(ubicacionId: Long?): Collection<Vector>
+    
+    @Query("FROM Vector v " +
+            "WHERE v.ubicacion.id = :idUbicacion")
+    fun vectoresEn(@Param("idUbicacion") ubicacionId: Long?): Collection<Vector>
 }
