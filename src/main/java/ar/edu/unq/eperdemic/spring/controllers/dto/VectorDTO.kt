794c5835ab597dc03c116304c2c2450fcb8dfe73
Fernando Romero <fer.rom.mu@gmail.com>
2023-05-12 14:14:11 -0300

    Paso tipo de vector a strings en contexto de dto vector.

--- src/main/java/ar/edu/unq/eperdemic/spring/controllers/dto/VectorDTO.kt
+++ src/main/java/ar/edu/unq/eperdemic/spring/controllers/dto/VectorDTO.kt
@@ -5,17 +5,17 @@ import ar.edu.unq.eperdemic.modelo.Ubicacion
 import ar.edu.unq.eperdemic.modelo.Vector
 
 
-class VectorDTO(val tipoDeVector : TipoDeVector,
+class VectorDTO(val tipoDeVector : String,
                 val ubicacion: Ubicacion) {
 
     fun aModelo() : Vector {
-        val vector = Vector(this.tipoDeVector)
+        val vector = Vector(TipoDeVector.valueOf(this.tipoDeVector))
         vector.ubicacion = this.ubicacion
         return vector
     }
 
     companion object {
-        fun desdeModelo(vector: Vector): VectorDTO = VectorDTO(vector.tipo, vector.ubicacion)
+        fun desdeModelo(vector: Vector): VectorDTO = VectorDTO(vector.tipo.toString(), vector.ubicacion)
     }
 
 }
