309ffec8391597e8fdcebcc40905643acbcba462
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2023-05-12 01:16:53 -0300

    implementacion endpoints de estadistica

--- src/main/java/ar/edu/unq/eperdemic/spring/controllers/EstadisticaControllerREST.kt
+++ src/main/java/ar/edu/unq/eperdemic/spring/controllers/EstadisticaControllerREST.kt
@@ -1,7 +1,10 @@
 package ar.edu.unq.eperdemic.spring.controllers
 
+import ar.edu.unq.eperdemic.services.EstadisticaService
+import ar.edu.unq.eperdemic.services.PatogenoService
 import ar.edu.unq.eperdemic.services.impl.EstadisticaServiceImpl
 import ar.edu.unq.eperdemic.spring.controllers.dto.EspecieDTO
+import ar.edu.unq.eperdemic.spring.controllers.dto.EspecieLiderDTO
 import ar.edu.unq.eperdemic.spring.controllers.dto.ReporteDeContagiosDTO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -15,13 +18,25 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/estadistica")
 class EstadisticaControllerREST {
 
-    @Autowired lateinit var estadisticaService: EstadisticaServiceImpl
+    @Autowired lateinit var estadisticaService: EstadisticaService
+    @Autowired lateinit var patogenoService : PatogenoService
 
     @GetMapping("/especieLider")
-    fun especieLider() = EspecieDTO.desdeModelo(estadisticaService.especieLider())
+    fun especieLider() : EspecieLiderDTO{
+        val especieLider = estadisticaService.especieLider()
+        val esPandemia = patogenoService.esPandemia(especieLider.id!!)
+
+        return EspecieLiderDTO.desdeModelo(especieLider.nombre, especieLider.patogeno.id, especieLider.vectores.size, esPandemia)
+    }
 
     @GetMapping("/lideres")
-    fun lideres(): List<EspecieDTO> = estadisticaService.lideres().map { EspecieDTO.desdeModelo(it) }
+    fun lideres(): List<EspecieLiderDTO> {
+        return estadisticaService.lideres().map {
+            val esPandemia = patogenoService.esPandemia(it.id!!)
+
+            EspecieLiderDTO.desdeModelo(it.nombre, it.patogeno.id, it.vectores.size, esPandemia)
+        }
+    }
 
     @GetMapping("/reporteDeContagios/{nombre}/{equipo}")
     fun repoteDeContagios(
