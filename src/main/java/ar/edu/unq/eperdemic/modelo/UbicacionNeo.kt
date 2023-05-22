295125c62708c6a8ebab7f3322452fdbab9741fe
Juan Cruz Insaurralde <juancruzinsaurralde@outlook.com>
2023-05-22 20:01:07 -0300

    ubicacionNeo repository

--- src/main/java/ar/edu/unq/eperdemic/modelo/UbicacionNeo.kt
+++ src/main/java/ar/edu/unq/eperdemic/modelo/UbicacionNeo.kt
@@ -1,7 +1,19 @@
 package ar.edu.unq.eperdemic.modelo
 
+import org.springframework.data.neo4j.core.schema.GeneratedValue
+import org.springframework.data.neo4j.core.schema.Id
 import org.springframework.data.neo4j.core.schema.Node
+import org.springframework.data.neo4j.core.schema.Relationship
 
 @Node
-class UbicacionNeo {
+class UbicacionNeo() {
+
+    @Id
+    @GeneratedValue
+    var id: Long? = null
+
+    var idUbicacion: Long? = null
+
+    @Relationship(type = "caminos")
+    var caminos : MutableSet<Camino> = HashSet()
 }
---
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/repository/neo/UbicacionNeoRepository.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.eperdemic.persistencia.repository.neo
+
+import ar.edu.unq.eperdemic.modelo.UbicacionNeo
+import org.springframework.data.neo4j.repository.Neo4jRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UbicacionNeoRepository : Neo4jRepository<UbicacionNeo, Long> {
+
+    fun conectados(nombreDeUbicacion:String): List<UbicacionNeo>
+
+    fun moverMasCorto(vectorId:Long, nombreDeUbicacion:String)
+}
