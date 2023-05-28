3916949260381e427bf5e81cc4a742b5daefc7bb
Fernando Romero <fer.rom.mu@gmail.com>
2023-05-28 18:35:07 -0300

    Se modificó vectorAleatorio de vector.

--- src/main/java/ar/edu/unq/eperdemic/persistencia/repository/spring/VectorRepository.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/repository/spring/VectorRepository.kt
@@ -2,6 +2,8 @@ package ar.edu.unq.eperdemic.persistencia.repository.spring
 
 import ar.edu.unq.eperdemic.modelo.Especie
 import ar.edu.unq.eperdemic.modelo.Vector
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.query.Param
@@ -15,10 +17,10 @@ interface VectorRepository : CrudRepository<Vector, Long> {
             "WHERE v.id = :idVector")
     fun enfermedades(@Param("idVector") id: Long?): List<Especie>
 
-    @Query("SELECT * FROM vector v " +
-            "JOIN ubicacion u ON u.id = :idUbi " +
-            "WHERE v.ubicacion_id = :idUbi " +
-            "order by rand() " +
-            "limit 1", nativeQuery = true)
-    fun vectorAleatorioEn(@Param("idUbi") ubicacionId: Long): Vector
+    @Query("""
+            from Vector v 
+            where v.ubicacion.id = :idUbi
+            order by rand()
+            """)
+    fun vectorAleatorioEn(@Param("idUbi") ubicacionId: Long, pageable: Pageable = PageRequest.of(0, 1)): List<Vector>
 }
