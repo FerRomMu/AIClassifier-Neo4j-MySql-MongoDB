eaef28a1d38d0a1927b8c3ec0a08c46acb8a1560
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2023-06-07 20:50:59 -0300

    creacion distrito repository

---
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/repository/mongo/IDistritoMongoRepository.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.eperdemic.persistencia.repository.mongo
+
+import ar.edu.unq.eperdemic.modelo.Distrito
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface IDistritoMongoRepository : MongoRepository<Distrito, String> {
+
+   // fun distritoMasEnfermo():Distrito
+}
