69ada3b444a014d67b449787d7df20abf33a0755
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2023-06-07 20:29:30 -0300

    se crea modelo de distrito y coordenada

---
+++ src/main/java/ar/edu/unq/eperdemic/modelo/Coordenada.kt
@@ -0,0 +1,10 @@
+package ar.edu.unq.eperdemic.modelo
+
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint
+
+class Coordenada(val latitud: Double, val longitud: Double) {
+
+    fun toGeoJsonPoint(): GeoJsonPoint {
+        return GeoJsonPoint(longitud, latitud)
+    }
+}
---
+++ src/main/java/ar/edu/unq/eperdemic/modelo/Distrito.kt
@@ -0,0 +1,21 @@
+package ar.edu.unq.eperdemic.modelo
+
+import org.springframework.data.mongodb.core.mapping.Document
+import javax.persistence.Id
+
+@Document("Distrito")
+class Distrito {
+
+    @Id
+    var id: String? = null
+    var nombre: String? = null
+    var coordenadas: MutableList<Coordenada> = mutableListOf()
+
+    protected constructor() {}
+
+    constructor(nombreD: String, coordenadasList: List<Coordenada>) {
+        coordenadas = coordenadasList.toMutableList()
+        nombre = nombreD
+    }
+
+}
