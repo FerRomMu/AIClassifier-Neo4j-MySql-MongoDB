3737784f8b73671bf8cdf7b0805d865d2621f982
Fernando Romero <fer.rom.mu@gmail.com>
2023-06-18 23:10:46 -0300

    Completo distritoMasEnfermo de service.

--- src/main/java/ar/edu/unq/eperdemic/services/impl/DistritoServiceimpl.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/DistritoServiceimpl.kt
@@ -1,7 +1,9 @@
 package ar.edu.unq.eperdemic.services.impl
 
+import ar.edu.unq.eperdemic.exceptions.DataNotFoundException
 import ar.edu.unq.eperdemic.modelo.Distrito
 import ar.edu.unq.eperdemic.persistencia.repository.mongo.DistritoMongoRepository
+import ar.edu.unq.eperdemic.persistencia.repository.mongo.UbicacionMongoRepository
 import ar.edu.unq.eperdemic.services.DistritoService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -11,13 +13,17 @@ class DistritoServiceimpl: DistritoService {
 
     @Autowired
     lateinit var distritoRepository: DistritoMongoRepository
+    @Autowired
+    lateinit var ubicacionRepository: UbicacionMongoRepository
 
     override fun crear(distrito: Distrito): Distrito {
         return distritoRepository.save(distrito)
     }
 
     override fun distritoMasEnfermo(): Distrito {
-        TODO("Not yet implemented")
+        val coordenadasEnfermas = ubicacionRepository.findCoordenadasConInfectados()
+        return distritoRepository.distritoMasEnfermo(coordenadasEnfermas) ?:
+            throw DataNotFoundException("No hay distrito con enfermos.")
     }
 
 }
