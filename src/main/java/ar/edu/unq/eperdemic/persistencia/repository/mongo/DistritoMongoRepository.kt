9e28edfe97cf3ffd55c8aaa9939eb3730bc0beee
Fernando Romero <fer.rom.mu@gmail.com>
2023-06-18 22:59:56 -0300

    Me traigo codigo de repository mongo a version estable.

--- src/main/java/ar/edu/unq/eperdemic/persistencia/repository/mongo/DistritoMongoRepository.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/repository/mongo/DistritoMongoRepository.kt
@@ -1,11 +1,14 @@
 package ar.edu.unq.eperdemic.persistencia.repository.mongo
 
+import ar.edu.unq.eperdemic.modelo.Coordenada
 import ar.edu.unq.eperdemic.modelo.Distrito
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface DistritoMongoRepository : MongoRepository<Distrito, String> {
 
-   // fun distritoMasEnfermo():Distrito
+    @Query("{ nombre: 'algo' }")
+    fun distritoMasEnfermo(coordenadas: List<Coordenada>):Distrito?
 }
--- src/main/java/ar/edu/unq/eperdemic/persistencia/repository/mongo/UbicacionMongoRepository.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/repository/mongo/UbicacionMongoRepository.kt
@@ -1,17 +1,25 @@
 package ar.edu.unq.eperdemic.persistencia.repository.mongo
 
 
+import ar.edu.unq.eperdemic.modelo.Coordenada
 import ar.edu.unq.eperdemic.modelo.UbicacionMongo
+import org.springframework.data.mongodb.repository.Aggregation
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UbicacionMongoRepository: MongoRepository<UbicacionMongo, String> {
+
     @Query("{nombre:'?0'}")
     fun findByNombre(nombre: String): UbicacionMongo
 
     @Query(value="{ 'nombre': { \$eq: ?0 }, 'coordenada': { \$nearSphere: { \$geometry: { type: 'Point', coordinates: [?2, ?1] }, \$maxDistance: ?3 } } }",exists = true)
     fun isLocationNearby(nombre: String, longitud: Double, latitud: Double, distancia: Double): Boolean
 
+    @Aggregation(pipeline = [
+        "{ \$match: { hayAlgunInfectado: true } }",
+        "{ \$project: { _id: 0, longitud: \"\$coordenada.longitud\", latitud: \"\$coordenada.latitud\" } }"
+    ])
+    fun findCoordenadasConInfectados(): List<Coordenada>
 }
